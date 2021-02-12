99c9f7edc3990e0e5c8a2569254a4d801152a456
Hammad-23 <hsyed2633@gmail.com>
2021-02-12 16:56:33 +0500

    working on C#

--- Program.cs
+++ Program.cs
@@ -184,7 +184,141 @@ namespace ConsoleApp2
             //Console.WriteLine("Area of rectangle is {0}", rectangle);
             //Console.WriteLine("Perimeter of rectangle is {0}", perimeter);
 
-            
+
+
+
+            //Console.WriteLine("Enter marks of subject 1");
+            //int marks1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter marks of subject 2");
+            //int marks2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter marks of subject 3");
+            //int marks3 = Convert.ToInt32(Console.ReadLine());
+
+            //double obtainedMarks = (double)marks1 + (double)marks2 + (double)marks3;
+            //double percentage = (obtainedMarks / 300) * 100;
+            //Console.WriteLine("percentage is--> {0}", percentage + "%");
+
+            //bool prob1 = percentage >= 80;
+            //bool prob2 = percentage >= 70 && percentage < 80;
+            //bool prob3 = percentage >= 60 && percentage < 70;
+            //bool prob4 = percentage >= 50 && percentage < 60;
+            //bool prob5 = percentage >= 40 && percentage < 50;
+
+
+            //switch (true)
+            //{
+            //    case prob1:
+            //        Console.WriteLine("You got A1");
+            //        break;
+
+            //    case prob2:
+            //        Console.WriteLine("You got A");
+            //        break;
+
+
+
+
+
+            //}
+
+
+            //int num1 = 2;
+            //int num2 = 4;
+            //num1 += 2;
+            //num2 -= 2;
+            //Console.WriteLine("num1--> {0}", num1);
+            //Console.WriteLine("num2--> {0}", num2);
+
+
+            //int number = 2;
+            //number = number + 2;
+            //Console.WriteLine(number);
+
+
+            //int number = 8;
+            //number /= 2;
+            //number /= 2;
+            //Console.WriteLine(number);
+
+
+            //int number = 8123;
+            //number %= 10;
+            //Console.WriteLine(number);
+
+
+            //int num = 10;
+            //num++;
+            //Console.WriteLine(num);
+            //num--;
+            //Console.WriteLine(num);
+
+
+            //int num = 4;
+            //Console.WriteLine(num++);
+            //Console.WriteLine(++num);
+
+
+
+
+            //int a = 10;
+            //int b, c;
+            //c = (b = a) + ++a;
+            //Console.WriteLine(c);
+
+
+            //int a = 10;
+            //int b = ++a + a++;
+            //Console.WriteLine(b);
+
+            //Console.WriteLine("enter first num");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter second number");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //bool check = num1 < num2;
+            //Console.WriteLine(check);
+
+            //Console.WriteLine("enter age");
+            //int age = int.Parse(Console.ReadLine());
+            //bool check = age >= 20 && age < 30;
+            //Console.WriteLine(check);
+
+
+
+            //Console.WriteLine("enter percentage");
+            //int per = Convert.ToInt32(Console.ReadLine());
+            //char check = per < 50 ? 'f' : per >= 50 && per < 60 ? 'c' : 'b';
+
+
+
+            //int num = 123;
+            //Console.WriteLine("num--> {0}", num);
+            //int first = num / 100;
+            //Console.WriteLine("first--> {0}", first);
+            //int r = num % 100;
+            //Console.WriteLine("r--> {0}", r);
+            //int second = r / 10;
+            //Console.WriteLine("second {0}", second);
+            //int third = num % 10;
+            //Console.WriteLine("third {0}", third);
+            //int sum = first + second + third;
+            //Console.WriteLine("sum--> {0}", sum);
+
+            //int number = 2;
+            //number *= 2;
+            //number *= 2;
+            //Console.WriteLine(number);
+
+
+
+            //int a, b, c, x;
+            //a = 80;
+            //b = 15;
+            //c = 2;
+
+            //x = a - b / (3 * c) * (a + c);
+
+
+            //Console.WriteLine(x);
 
 
 
@@ -196,11 +330,107 @@ namespace ConsoleApp2
 
 
 
+            //Q1
+
+
+            //Console.WriteLine("Enter your total amount");
+            //double worth = Convert.ToDouble(Console.ReadLine());
+            //double zakat = worth * (2.5 / 100);
+            //Console.WriteLine("You have to pay {0} for zakat", zakat);
+
+
+
+            //Q2
+
+
+            //Console.WriteLine("Enter miles value");
+            //double miles = Convert.ToDouble(Console.ReadLine());
+            //double meters = miles * 1609.4;
+            //Console.WriteLine("value in meter is {0}", meters);
 
 
 
 
 
-        }
+            //Q3
+
+
+
+            //Console.WriteLine("Enter basic salary");
+            //double basicSalary = Convert.ToDouble(Console.ReadLine());
+            //double allowance = (basicSalary / 100) * 40;
+            //Console.WriteLine("allowance--> {0}", allowance);
+            //double rent = (basicSalary / 100) * 20;
+            //Console.WriteLine("rent--> {0}", rent);
+            //double grossSalary = basicSalary + allowance + rent;
+            //Console.WriteLine("Gross Salary is {0}", grossSalary);
+
+
+
+
+
+            //int a, b, c, x;
+            //a = 80;
+            //b = 15;
+            //c = 2;
+
+            //x = a - b / (3 * c) * (a + c);
+            //Console.WriteLine(x);
+
+
+
+            //int a = 5, b = 6, c;
+            //c = a;
+            //a = b;
+            //b = c;
+
+            //Console.WriteLine(a + " " + b);
+
+
+            //Console.WriteLine("please enter mass of an object");
+            //double mass = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter acceleration");
+            //double acceleration = Convert.ToDouble(Console.ReadLine());
+            //double force = mass * acceleration;
+            //Console.WriteLine(force);
+
+
+
+            //Console.Write("Enter starting number");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter range");
+            //int range = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //for(int i=number; i<=range; i++)
+            //{
+            //    sum = i + sum;
+
+            //}
+
+            //Console.WriteLine("Sum {0}", sum);
+
+
+            Console.Write("Enter number ");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 5 == 0)
+            {
+                Console.WriteLine("Number is divisible by 5 ");
+            }else if (num % 7 == 0)
+            {
+                Console.WriteLine("Number is divisible by 7 ");
+            }
+            else
+            {
+                Console.WriteLine("Number is not divisible by 5 or even 7 ");
+            }
+
+
+
+
+
+
+
+
+            }
     }
 }
